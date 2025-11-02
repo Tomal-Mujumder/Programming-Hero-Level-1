bf1159f58c0d984d42f60ff2f2db0deeec9447fe
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-02 08:53:23 +0600

    arrow function done

--- Programming_Hero/javascript_Learning/function3.js
+++ Programming_Hero/javascript_Learning/function3.js
@@ -1,2 +1,10 @@
 const prompt = require('prompt-sync')();
 
+let takeinfo = () =>
+{
+    console.log("Enter your name:");
+    let name = prompt();
+    return name;
+}
+let First_Name = takeinfo();
+console.log(First_Name);
