2b531567821ae0376120ebd427bcc2fe3320c78a
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-03 10:52:59 +0600

    object practice

--- Programming_Hero/javascript_Learning/shallowcopy.js
+++ Programming_Hero/javascript_Learning/shallowcopy.js
@@ -25,7 +25,7 @@
 // deepCopy.name = "jay";
 // console.log(`${person.name}`);
 
-const prompt = require(`prompt-sync`)();
+const prompt = require("prompt-sync")();
 let student = {
     name:prompt(),
     roll:parseInt(prompt()), //used parseInt to make input integer
@@ -34,9 +34,20 @@ let student = {
         parent_1:parseInt(prompt()),
         parent_2:parseInt(prompt())
     }
-}
+};
 
 let course_taken ={
     courseId:parseInt(prompt()),
     ...student
+};
+console.log(`If you want to change something then write yes\n`);
+var choice = prompt();
+if(choice === "yes")
+{
+    console.log("Enter your parents mobile number:");
+    course_taken.mobile_Number.parent_1 = parseInt(prompt());
+    course_taken.mobile_Number.parent_2 = parseInt(prompt());
 }
+else console.log("you are registered");
+
+console.log(student);
