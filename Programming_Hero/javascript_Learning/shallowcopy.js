eeea66376c2e13a6e022ad5eab88742721f937e2
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-03 10:31:50 +0600

    new methods trying

--- Programming_Hero/javascript_Learning/shallowcopy.js
+++ Programming_Hero/javascript_Learning/shallowcopy.js
@@ -1,26 +1,42 @@
 //concept of copying array or object in js and how they store them using stack
 //and heap memory
 
-const prompt = require('prompt-sync')();
+// const prompt = require('prompt-sync')();
 
-let person = {
+// let person = {
 
-    name:prompt("Enter your name:"),
-    roll: prompt("Enter your roll:"),
-    section: prompt("Enter your section:"),
-    address:
-    {
-        city:"dhaka",
-        postal_code:"1230"
-    }
-}
-console.log(`${person.name} ${person.roll} ${person.section}`);
+//     name:prompt("Enter your name:"),
+//     roll: prompt("Enter your roll:"),
+//     section: prompt("Enter your section:"),
+//     address:
+//     {
+//         city:"dhaka",
+//         postal_code:"1230"
+//     }
+// }
+// console.log(`${person.name} ${person.roll} ${person.section}`);
 
-let shallowCopy = {...person};
-shallowCopy.address.postal_code = "jay";
-console.log(`${person.address.postal_code}`);
+// let shallowCopy = {...person};
+// shallowCopy.address.postal_code = "jay";
+// console.log(`${person.address.postal_code}`);
 
 // let deepCopy = JSON.parse(JSON.stringify(person));
 
 // deepCopy.name = "jay";
 // console.log(`${person.name}`);
+
+const prompt = require(`prompt-sync`)();
+let student = {
+    name:prompt(),
+    roll:parseInt(prompt()), //used parseInt to make input integer
+    section:prompt(),
+    mobile_Number:{
+        parent_1:parseInt(prompt()),
+        parent_2:parseInt(prompt())
+    }
+}
+
+let course_taken ={
+    courseId:parseInt(prompt()),
+    ...student
+}
