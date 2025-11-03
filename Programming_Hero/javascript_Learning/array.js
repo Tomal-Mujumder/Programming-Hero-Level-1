cf3e3fa92025e59da7d84b48db34ee03c14d8657
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-03 11:34:59 +0600

    methods practicing

--- Programming_Hero/javascript_Learning/array.js
+++ Programming_Hero/javascript_Learning/array.js
@@ -3,12 +3,20 @@
 const prompt = require("prompt-sync")();
 var studentRoll = [1,2,3,4,5]
 
-var studentName = {
-    student1:prompt(),
-    student2:prompt()
-};
-
-console.log(studentRoll.length);
+// var studentName = {
+//     student1:prompt(),
+//     student2:prompt()
+// };
 
 studentRoll.push(19,29,39);
+//console.log(studentRoll);
+//console.log(studentRoll.length);
+
+//studentRoll.pop();
+//studentRoll.shift();
 console.log(studentRoll);
+
+studentRoll.unshift(20,10,10);
+console.log(studentRoll);
+
+console.log(studentRoll.length);
