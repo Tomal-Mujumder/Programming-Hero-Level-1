304190732ff1767faefdc672ba68e197d7416e95
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-03 12:28:31 +0600

    find updated

--- Programming_Hero/javascript_Learning/array2.js
+++ Programming_Hero/javascript_Learning/array2.js
@@ -1,30 +1,30 @@
 
-let me = ["tomal","maisha","kids"]
+// let me = ["tomal","maisha","kids"]
 
-var searchIndex=0;
+// var searchIndex=0;
 
-var search = me.find((yo,index)=>
-{
+// var search = me.find((yo,index)=>
+// {
 
-    if(yo==="maisha")
-        {
-            searchIndex=index;
-            return yo;
-        }
-});
-console.log(search);
-console.log(searchIndex);
-// const users = [
-//   { name: 'Alice', age: 20 },
-//   { name: 'Bob', age: 25 },
-//   { name: 'Charlie', age: 30 }
-// ];
-
-// // The callback function returns true for the first matching element
-// const firstAdult = users.find( (user, index) => {
-//   console.log(`Checking index ${index}: ${user.age}`);
-//   return user.age >= 25; // Condition to test
+//     if(yo==="maisha")
+//         {
+//             searchIndex=index;
+//             return yo;
+//         }
 // });
+// console.log(search);
+// console.log(searchIndex);
 
-// console.log(firstAdult); 
-// // Output: { name: 'Bob', age: 25 }
+var family = ["tomal", "maisha", "kids"]
+
+let searchIndex = 0;
+
+var search = family.find((search,index)=>
+{
+    if(search === "maisha")
+    {
+        searchIndex = index;
+        return search;
+    }
+});
+console.log(`Her name is ${search} and positon at array ${searchIndex}`);
