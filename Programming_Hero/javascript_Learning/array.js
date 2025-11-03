61ea15e8a1019ebe201e2ed8e0ae1566bdc565b0
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-03 11:49:34 +0600

    indexof done

--- Programming_Hero/javascript_Learning/array.js
+++ Programming_Hero/javascript_Learning/array.js
@@ -1,25 +1,30 @@
 
 
-const prompt = require("prompt-sync")();
-var studentRoll = [1,2,3,4,5]
+// const prompt = require("prompt-sync")();
+// var studentRoll = [1,2,3,4,5]
 
-// var studentName = {
-//     student1:prompt(),
-//     student2:prompt()
-// };
+// // var studentName = {
+// //     student1:prompt(),
+// //     student2:prompt()
+// // };
 
-studentRoll.push(19,29,39);
-//console.log(studentRoll);
-//console.log(studentRoll.length);
+// studentRoll.push(19,29,39);
+// //console.log(studentRoll);
+// //console.log(studentRoll.length);
 
-//studentRoll.pop();
-//studentRoll.shift();
+// //studentRoll.pop();
+// //studentRoll.shift();
 
-studentRoll.unshift(20,10,10);
+// studentRoll.unshift(20,10,10);
 
-let search = studentRoll.includes(39);
-if(search)console.log("present\n");
-else console.log("absent\n");
-console.log(studentRoll);
+// let search = studentRoll.includes(39);
+// if(search)console.log("present\n");
+// else console.log("absent\n");
+// console.log(studentRoll);
 
-console.log(studentRoll.length);
+// console.log(studentRoll.length);
+
+let me = ['tomal','maisha','kids']
+
+var searchOfindex = me.indexOf('maisha');
+console.log(searchOfindex);
