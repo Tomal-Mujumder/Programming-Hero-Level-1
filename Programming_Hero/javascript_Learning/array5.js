e29e1bf13a1333c9017fee88abff7d5181b0b6b5
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-04 16:13:50 +0600

    map retest

--- Programming_Hero/javascript_Learning/array5.js
+++ Programming_Hero/javascript_Learning/array5.js
@@ -1,11 +1,20 @@
 
-var array = [{name:"milestone",address:"sector-6",studenNumber:2330},
-    {name:"rajuk",address:"sector-7",studentNumber:3330}]
+// var array = [{name:"milestone",address:"sector-6",studenNumber:2330},
+//     {name:"rajuk",address:"sector-7",studentNumber:3330}]
 
-var search = array.map((value,index,response)=>
+// var search = array.map((value,index,response)=>
+// {
+//     const greater = value.studentNumber<3000;
+//     return greater;
+// });
+// console.log(search);
+// console.log(array[1]);
+
+var ami = [{name:"tomal", age:50}, {name:"mim",age:35}]
+
+var search = ami.map((value,index,arr)=>
 {
-    const greater = value.studentNumber<3000;
-    return greater;
+    if(value.age>40)return value;
+    else return 0;
 });
 console.log(search);
-console.log(array[1]);
