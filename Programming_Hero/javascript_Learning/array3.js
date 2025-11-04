18e730c3017bdee7cf0c433e68203531eede8c40
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-04 11:48:01 +0600

    arraysorting done

--- Programming_Hero/javascript_Learning/array3.js
+++ Programming_Hero/javascript_Learning/array3.js
@@ -11,3 +11,10 @@
 // var search = me.sort((a,b)=>a.age-b.age);
 // console.log(search);
 
+let search = [{ProductName:"watch",Productcatagory:"Men",ProductPrice:"2000tk"},
+    {ProductName:"Graphics Tablet", Productcatagory:"Unisex",ProductPrice:"3900"},
+    {ProductName:"Headset",ProductCatagory:"Unisex",ProductPrice:"1400"}
+]
+
+var arraySorting = search.sort((a,b)=>a.ProductPrice-b.ProductPrice);
+console.log(arraySorting);
