aa1511f6ee66a30acff9ceec80cb449f7a4f83dc
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-04 14:20:34 +0600

    slice test

--- Programming_Hero/javascript_Learning/array4.js
+++ Programming_Hero/javascript_Learning/array4.js
@@ -1,16 +1,22 @@
 
-var array = [{name:"tomal",age:23},
-    {name:"maisha",age:21},
-    {name:"arnob",age:3}
+// var array = [{name:"tomal",age:23},
+//     {name:"maisha",age:21},
+//     {name:"arnob",age:3}
+// ]
+// let searchindex;
+// var search = array.find((value,index)=>
+// {
+//     if(value.name==="arnob")
+//     {
+//         searchindex = index;
+//         return value;
+//     }
+// });
+// console.log(`Index Number: ${searchindex}`);
+// console.log(search);
+
+var me = [{name:"tomal",age:23,passion:"software engineering"},
+    {name:"maisha",age:21,passion:"watching movie"}
 ]
-let searchindex;
-var search = array.find((value,index)=>
-{
-    if(value.name==="arnob")
-    {
-        searchindex = index;
-        return value;
-    }
-});
-console.log(`Index Number: ${searchindex}`);
-console.log(search);
+var mapTest = me.slice(0,1);
+console.log(mapTest);
