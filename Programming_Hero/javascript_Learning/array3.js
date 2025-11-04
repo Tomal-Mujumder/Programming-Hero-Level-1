c8240a70a1ad7310bf2df68832b7e0171050f793
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-04 11:40:49 +0600

    array3.js updated

--- Programming_Hero/javascript_Learning/array3.js
+++ Programming_Hero/javascript_Learning/array3.js
@@ -1,6 +1,13 @@
 
-var mai = [40,3,69]
+// var mai = [40,3,69]
 
-var search = mai.sort((a,b)=>a-b);
+// var search = mai.sort((a,b)=>a-b);
 
-console.log(search);
+// console.log(search);
+
+// var me = [{name:"Tomal",religion:"Hindu",age:27},
+//     {name:"Maisha",religion:"Islam",age:21},
+//     {name:"arti",religion:"beliver",age:3}]
+// var search = me.sort((a,b)=>a.age-b.age);
+// console.log(search);
+
