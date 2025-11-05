138074dd11399469d888f2abc54023af87f112fc
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-05 09:23:21 +0600

    practicing reduce

--- Programming_Hero/javascript_Learning/array6.js
+++ Programming_Hero/javascript_Learning/array6.js
@@ -1,8 +1,17 @@
 
-var array = [1,2,3,4,5]
+// var array = [1,2,3,4,5]
 
-var testReduce = array.reduce((holdsResult,currentValue)=>{
-            return holdsResult+currentValue;
-},0);
+// var testReduce = array.reduce((holdsResult,currentValue)=>{
+//             return holdsResult+currentValue;
+// },0);
 
-console.log(testReduce);
+// console.log(testReduce);
+
+var array = [{name:"tomal",age:25},{name:"asif",age:23}]
+
+var testingReduce = array.reduce((holds,current)=>{
+    if(current.name==="asif") return current;
+    else return "null";
+},{});
+
+console.log(testingReduce);
