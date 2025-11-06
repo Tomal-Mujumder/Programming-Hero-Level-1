0488c73c7680b2eebcb114467d2202085d7bcf55
Pandas-love <jaymujumderiubat@gmail.com>
2025-11-06 10:54:37 +0600

    spread done

--- Programming_Hero/javascript_Learning/spread.js
+++ Programming_Hero/javascript_Learning/spread.js
@@ -1,11 +1,45 @@
 
-const person = ["tomal","jay","asif"];
+// const person = ["tomal","jay","asif"];
 
-const copyperson = [...person];
-console.log(copyperson);
+// const copyperson = [...person];
+// console.log(copyperson);
 
-function me(a,b,c)
+// function me(a,b,c)
+// {
+//     console.log(`${a}+${b}+${c}`);
+// }
+// me(...copyperson);
+
+var starcopy = ["me", "myself", "you"]
+
+var distar = ["short", "cute", "golu"]
+
+var how = [...starcopy,...distar]
+
+//using arrow function and foreach
+
+// how.forEach(element => {
+//     console.log(element);
+// });
+
+// function me(a,b,c)
+// {
+//     console.log(`${a} ${b} ${c}`);
+// }
+
+// me(...distar);
+
+var studentInfo = {name:"tomal",id:2,age:25,profession:"IT"};
+
+var newStudentList = {...studentInfo};
+
+//Object.values extract value of object property and store as an array
+
+var array = Object.values(newStudentList);
+
+function obj(a,b,c)
 {
-    console.log(`${a}+${b}+${c}`);
+    console.log(`${a} ${b} ${c}`);
 }
-me(...copyperson);
+
+obj(...array);
